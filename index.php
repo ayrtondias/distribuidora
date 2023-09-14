2ef60dac03a5722305e38ae9d05745b9443418b0
ayrtondias <ayrton.dias68@gmail.com>
2023-09-13 21:58:50 -0300

    tele de produtos

--- index.php
+++ index.php
@@ -20,59 +20,84 @@
         </div>
     </div>
   
-    
 
 
-  <div class="pos-f-t">
-  <div class="collapse" id="navbarToggleExternalContent">
-    <div class="bg-dark p-4">
-      <h5 class="text-white h4">Conteúdo expandido</h5>
-      <span class="text-muted">Expansível, atráves da marca no navbar.</span>
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #FB7226;">
+  <div class="dropdown">
+    <button class="btn btn-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="background-color: #FB7226; border: none;">
+      <span class="navbar-toggler-icon"></span>
+      CATEGORIAS
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item" href="#">PISOS E REVESTIMENTOS</a>
+      <hr>
+      <a class="dropdown-item" href="#">FERRAMENTAS</a>
+      <hr>
+      <a class="dropdown-item" href="#">ELÉTRICA</a>
+      <hr>
+      <a class="dropdown-item" href="#">ASSETAMENTO E REJUNTAMENTO</a>
+      <hr>
+      <a class="dropdown-item" href="#">BANHEIRO</a>
+      <hr>
+      <a class="dropdown-item" href="#">COZINHA E LAVANDERIA</a>
+      <hr>
+      <a class="dropdown-item" href="#">FERRAGENS</a>
+      <hr>
+      <a class="dropdown-item" href="#">HIDRÁULICA</a>
+      <hr>
+      <a class="dropdown-item" href="#">ILUMINAÇÃO</a>
+      <hr>
+      <a class="dropdown-item" href="#">PINTURA</a>
+      <hr>
+      <a class="dropdown-item" href="#">PORTAS E JANELAS</a>
     </div>
   </div>
-  <nav class="navbar navbar-dark" style="background-color: #FB7226;" id="barra">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Alterna navegação">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  </nav>
-</div>
-
-<nav class="navbar  navbar-dark" style="background-color: #FB7226;">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Destaques</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Preços</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Desativado</a>
-      </li>
-    </ul>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+      <li class="nav-item">
+        <a class="nav-link" href="#">PISOS E REVESTIMENTOS</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Destaques</a>
+        <a class="nav-link" href="#">FERRAMENTAS</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Preços</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Desativado</a>
+        <a class="nav-link" href="#">ELÉTRICA</a>
       </li>
     </ul>
   </div>
 </nav>
-    
+
+<h1 class="cat">PISOS E REVESTIMENTOS</h1>
+
+<div class="corpo"> 
+<div class="subCat">
+  <ul class="textop">Sub-categorias
+
+    <li class="texcorpo">Porcelanato ()</li>
+    <li class="texcorpo"> ()</li>
+    <li class="texcorpo"> ()</li>
+    <li class="texcorpo"> ()</li>
+    <li class="texcorpo"> ()</li>
+  </ul>
+  <br>
+  <ul class="textop">Marcas
+    <li class="texcorpo"> ()</li>
+    <li class="texcorpo"> ()</li>
+    <li class="texcorpo"> ()</li>
+    <li class="texcorpo"> ()</li>
+  </ul>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
+  <div class="card-body">
+    <h5 class="card-title">Título do card</h5>
+    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+    <a href="#" class="btn btn-primary">Visitar</a>
+  </div>
+</div>
+</div>
+
     
 </body>
 </html>
