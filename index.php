eb5155fd70bf884ddedd6ffe31be7e2bf2da0d51
ayrtondias <ayrton.dias68@gmail.com>
2023-09-25 22:04:23 -0300

    att

--- index.php
+++ index.php
@@ -90,13 +90,13 @@
   </ul>
 </div>
 
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src=".../100px180/" alt="Imagem de capa do card">
-  <div class="card-body">
-    <h5 class="card-title">Título do card</h5>
-    <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
-    <a href="#" class="btn btn-primary">Visitar</a>
+<div class="card">
+  <img class="imagem" src=".../100px180/" alt="Imagem de capa do card">
+  <label class="card_text">Nome do Produto</label>
+
   </div>
+
+
 </div>
 </div>
 
