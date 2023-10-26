dee91a3eff44a7995927378b4ab9a77a0848ad27
ayrtondias <ayrton.dias68@gmail.com>
2023-10-25 21:59:22 -0300

    att

---
+++ minha_conta.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style_conta.css">
+    <title>Minha cesta</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="topo">
+      <img class="logo" src="img/dp.png">      
+    </div>
+    <div class="corpo">
+        <div class="opcoes">
+            <button class="btn_opcoes">
+                <img class="icones" src="img/icones/172628_user_male_icon.png" alt="">
+                Minha conta
+
+            </button>
+
+            <button class="btn_opcoes" style="margin-top: 5px;">
+                <img src="" alt="">
+                Pedidos
+
+            </button>
+
+            <button class="btn_opcoes" style="margin-top: 5px;">
+                <img src="" alt="">
+                Favoritos
+
+            </button>
+
+            <button class="btn_opcoes" style="margin-top: 5px;">
+                <img src="" alt="">
+                Vales
+
+            </button>
+
+            <button class="btn_opcoes" style="margin-top: 5px;">
+                <img src="" alt="">
+                Endereços
+
+            </button>
+
+
+        </div>
+        <div class="tela">            
+            
+        </div>  
+    </div>
+
+    
+</body>
+</html>
